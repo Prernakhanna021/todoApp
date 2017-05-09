ce7bd9a2aaac7c8659d42755da5b0e0623ec5de5
Prerna Khanna <pk462@njit.edu>
2017-05-08 23:18:30 -0400

    bug fix for db connectivity

--- database_connection.php
+++ database_connection.php
@@ -1,7 +1,7 @@
 <?php
   $dsn = 'mysql:host=sql1.njit.edu;dbname=pk462';
-  $username='pk462';
-  $password='macBook2016';
+  $db_username='pk462';
+  $db_password='macBook2016';
 
   try{
     $db = new PDO($dsn,$db_username,$db_password);
